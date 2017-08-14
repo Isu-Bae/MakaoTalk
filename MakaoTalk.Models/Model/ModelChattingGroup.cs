94f83405070c3c80e90bd7f4fd495b54851bb95d
lewisjhon <happin0202@naver.com>
2017-08-14 21:07:52 +0900

    Change Model Namespace

--- MakaoTalk.Models/Model/ModelChattingGroup.cs
+++ MakaoTalk.Models/Model/ModelChattingGroup.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MakoTalk.Models.ViewModel
+namespace MakoTalk.Models.Model
 {
     public enum GroupTypes
     {
--- MakaoTalk.Models/Model/ModelUser.cs
+++ MakaoTalk.Models/Model/ModelUser.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MakoTalk.Models.ViewModel
+namespace MakoTalk.Models.Model
 {
     public class ModelUser : DomainModel
     {
